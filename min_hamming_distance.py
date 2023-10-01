2ce4760df08312b0f2e5ddd517241a2063140e26
Sushant T <sushantt@Sushants-MacBook-Air-2.local>
2023-10-01 15:13:07 -0500

    Added the kmer which causes the minimum hamming distance

--- min_hamming_distance.py
+++ min_hamming_distance.py
@@ -4,9 +4,11 @@ the following function gives the minimum hamming distance between the pattern an
 from hamming_distance import hamming_distance
 def min_hamming_distance(pattern,text):
     min_hamming_distance = len(text)
+    min_hamming_kmer = "" # this is the kmer where the minimum hamming distance occurs
     for i in range(len(pattern)-len(text)+1): 
         i_kmer = (pattern[i:i+len(text)])
         i_hamming_distance = hamming_distance(i_kmer,text)
         if i_hamming_distance<min_hamming_distance:
             min_hamming_distance = i_hamming_distance
-    return min_hamming_distance
+            min_hamming_kmer = i_kmer
+    return [min_hamming_distance,min_hamming_kmer]
